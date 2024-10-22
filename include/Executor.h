b3b825225134eff37888d9d2a3e2c1597e4fd7a5
mynameisczj <czjwangyi2023@163.com>
2024-10-22 19:59:51 +0800

    style: use google style rebuild the code

--- include/Executor.h
+++ include/Executor.h
@@ -1,36 +1,33 @@
 #pragma once
 #include <string>
-namespace adas
-{
-    /*Car Data*/
-    struct Pose
-    {
-        int x, y;     //[x,y]坐标
-        char heading; //[N,S,E,W]四个方向
-    };
-    /*
-        驾驶动作执行接口
-    */
-    class Executor
-    {
-    public:
-        static Executor *NewExecutor(const Pose &pose = {0, 0, 'N'}) noexcept;
+namespace adas {
+/*Car Data*/
+struct Pose {
+  int x, y;      //[x,y]坐标
+  char heading;  //[N,S,E,W]四个方向
+};
+/*
+    驾驶动作执行接口
+*/
+class Executor {
+ public:
+  static Executor *NewExecutor(const Pose &pose = {0, 0, 'N'}) noexcept;
 
-    public:
-        // 默认构造函数
-        Executor(void) = default;
-        // 默认虚构函数
-        virtual ~Executor(void) = default;
-        // 禁止拷贝
-        Executor(const Executor &) = delete;
-        // 禁止赋值
-        Executor &operator=(const Executor &) = delete;
+ public:
+  // 默认构造函数
+  Executor(void) = default;
+  // 默认虚构函数
+  virtual ~Executor(void) = default;
+  // 禁止拷贝
+  Executor(const Executor &) = delete;
+  // 禁止赋值
+  Executor &operator=(const Executor &) = delete;
 
-    public:
-        // 查询当前汽车姿态(接口)
-        virtual Pose Query(void) const noexcept = 0;
-        // 执行指令方法(接口)
-        virtual void Execute(const std::string &commands) noexcept = 0;
-    };
+ public:
+  // 查询当前汽车姿态(接口)
+  virtual Pose Query(void) const noexcept = 0;
+  // 执行指令方法(接口)
+  virtual void Execute(const std::string &commands) noexcept = 0;
+};
 
-} // namespace adas
+}  // namespace adas
--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -1,113 +1,87 @@
 #include "ExecutorImpl.h"
-#include <new>
-#include<memory>
-namespace adas
-{
-    ExecutorImpl::ExecutorImpl(const Pose &pose) noexcept : pose(pose) {}
 
-    Pose ExecutorImpl::Query(void) const noexcept
-    {
-        return pose;
+#include <memory>
+#include <new>
+namespace adas {
+ExecutorImpl::ExecutorImpl(const Pose &pose) noexcept : pose(pose) {}
+
+Pose ExecutorImpl::Query(void) const noexcept { return pose; }
+/*
+    std::nothrow std::nothrow_t 类型实例,
+    当内存分配失败时返回空指针而不是 std::bad_alloc 异常
+*/
+Executor *Executor::NewExecutor(const Pose &pose) noexcept {
+  return new (std::nothrow) ExecutorImpl(pose);  // C++17下有效
+}
+void ExecutorImpl::Execute(const std::string &commands) noexcept {
+  for (const auto cmd : commands) {
+    std::unique_ptr<ICommand> cmder;
+    // 指令是M指令
+    if (cmd == 'M') {
+      cmder = std::make_unique<MoveCommand>();
     }
-    /*
-        std::nothrow std::nothrow_t 类型实例,
-        当内存分配失败时返回空指针而不是 std::bad_alloc 异常
-    */
-    Executor *Executor::NewExecutor(const Pose &pose) noexcept
-    {
-        return new (std::nothrow) ExecutorImpl(pose); // C++17下有效
+    // 指令是L指令
+    if (cmd == 'L') {
+      cmder = std::make_unique<TurnLeftCommand>();
     }
-    void ExecutorImpl::Execute(const std::string &commands) noexcept
-    {
-        for (const auto cmd : commands)
-        {
-            std::unique_ptr<ICommand> cmder;
-            // 指令是M指令
-            if (cmd == 'M')
-            {
-                cmder = std::make_unique<MoveCommand>();
-            }
-            // 指令是L指令
-            if (cmd == 'L')
-            {
-                cmder = std::make_unique<TurnLeftCommand>();
-            }
-            // 指令是R指令
-            if (cmd == 'R')
-            {
-                cmder = std::make_unique<TurnRightCommand>();
-            }
-            //指令是F指令
-            if (cmd == 'F')
-            {
-                cmder = std::make_unique<FastCommand>();
-            }
-            if(cmder)
-            {
-                cmder->DoOperate(*this);
-            }
-            // end
-        }
+    // 指令是R指令
+    if (cmd == 'R') {
+      cmder = std::make_unique<TurnRightCommand>();
     }
-    void ExecutorImpl::Move() noexcept{
-        for(auto i=0;i<=fast;i++)
-        if (pose.heading == 'E'){
-            ++pose.x;
-        }
-        else if (pose.heading == 'W'){
-            --pose.x;
-        }
-        else if (pose.heading == 'N'){
-            ++pose.y;
-        }
-        else if (pose.heading == 'S'){
-            --pose.y;
-        }
+    // 指令是F指令
+    if (cmd == 'F') {
+      cmder = std::make_unique<FastCommand>();
     }
-    void ExecutorImpl::TurnLeft() noexcept{
-        if(fast)
-        {
-            fast=false;
-            Move();
-            fast=true;
-        }
-        if (pose.heading == 'E'){
-            pose.heading = 'N';
-        }
-        else if (pose.heading == 'W'){
-            pose.heading = 'S';
-        }
-        else if (pose.heading == 'N'){
-            pose.heading = 'W';
-        }
-        else if (pose.heading == 'S'){
-            pose.heading = 'E';
-        }
+    if (cmder) {
+      cmder->DoOperate(*this);
     }
-    void ExecutorImpl::TurnRight() noexcept{
-        if(fast)
-        {
-            fast=false;
-            Move();
-            fast=true;
-        }
-        if (pose.heading == 'E'){
-            pose.heading = 'S';
-        }
-        else if (pose.heading == 'W'){
-            pose.heading = 'N';
-        }
-        else if (pose.heading == 'N'){
-            pose.heading = 'E';
-        }
-        else if (pose.heading == 'S'){
-            pose.heading = 'W';
-        }
+    // end
+  }
+}
+void ExecutorImpl::Move() noexcept {
+  for (auto i = 0; i <= fast; i++)
+    if (pose.heading == 'E') {
+      ++pose.x;
+    } else if (pose.heading == 'W') {
+      --pose.x;
+    } else if (pose.heading == 'N') {
+      ++pose.y;
+    } else if (pose.heading == 'S') {
+      --pose.y;
     }
-    void ExecutorImpl::Fast() noexcept{
-        fast=(fast?false:true);
-    }
-    bool ExecutorImpl::IsFast() noexcept{
-        return this->fast;
-    }
-} // namespace adas
+}
+void ExecutorImpl::TurnLeft() noexcept {
+  if (fast) {
+    fast = false;
+    Move();
+    fast = true;
+  }
+  if (pose.heading == 'E') {
+    pose.heading = 'N';
+  } else if (pose.heading == 'W') {
+    pose.heading = 'S';
+  } else if (pose.heading == 'N') {
+    pose.heading = 'W';
+  } else if (pose.heading == 'S') {
+    pose.heading = 'E';
+  }
+}
+void ExecutorImpl::TurnRight() noexcept {
+  if (fast) {
+    fast = false;
+    Move();
+    fast = true;
+  }
+  if (pose.heading == 'E') {
+    pose.heading = 'S';
+  } else if (pose.heading == 'W') {
+    pose.heading = 'N';
+  } else if (pose.heading == 'N') {
+    pose.heading = 'E';
+  } else if (pose.heading == 'S') {
+    pose.heading = 'W';
+  }
+}
+void ExecutorImpl::Fast() noexcept { fast = (fast ? false : true); }
+bool ExecutorImpl::IsFast() noexcept { return this->fast; }
+}  // namespace adas
